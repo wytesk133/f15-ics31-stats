0209e8e04984a8777bb9c7b7133fe96066275709
Waitaya Krongapiradee <wyte.sk133@gmail.com>
2016-01-30 22:01:04 -0800

    Add midterm scores

--- plot.py
+++ plot.py
@@ -1,6 +1,6 @@
 GRADES = ['A+', 'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D+', 'D', 'D-', 'F', 'P', 'NP', 'NR']
 
-def read_from_file(path:str)->('x', 'y'):
+def extract_final(path:str)->('x', 'y'):
     data = open(path)
     count = 0
     skipped = 0
@@ -26,16 +26,35 @@ def read_from_file(path:str)->('x', 'y'):
         value = item[1]
         x.append(key)
         y.append(value)
-        print('{:2} => {}'.format(key, len(value)))
     data.close()
-    print('Processed: {}/{}'.format(count, count + skipped))
+    print('Processed: {} (Skipped: {})'.format(count, skipped))
+    return x, y
+
+def extract_midterm(path:str)->('x', 'y'):
+    data = open(path)
+    x = []
+    y = []
+    count = 0
+    skipped = 0
+    for line in data:
+        l = line.replace('[', ' ').replace(']', ' ').split()
+        if(l[0][-1] == '-'):
+            skipped += int(l[1])
+            continue
+        x.append(int(float(l[0])))
+        y.append(int(l[1]))
+        count += int(l[1])
+    x.reverse()
+    y.reverse()
+    data.close()
+    print('Processed: {} (Skipped: {})'.format(count, skipped))
     return x, y
 
 def render():
     html = '''<!DOCTYPE html>
 <html>
   <head>
-    <title>ICS 31 Fall 2015 Grade Distribution</title>
+    <title>ICS 31 Fall 2015 Statistics</title>
     <meta charset="utf-8">
     <meta name="author" content="Waitaya Krongapiradee">
     <link rel="stylesheet" href="https://necolas.github.io/normalize.css/3.0.2/normalize.css">
@@ -43,31 +62,46 @@ def render():
     <script type="text/javascript" src="http://numericjs.com/lib/numeric-1.2.6.min.js"></script>
   </head>
   <body>
-    <div style="width: 80%; margin: auto">
+    <div style="max-width: 1024px; margin: auto">
         <div id="boxPlot"></div>
         <div id="gradeDistribution"></div>
+        <div id="midterm1"></div>
+        <div id="midterm2"></div>
     </div>
     <script type="text/javascript">
-      var xData = {}
-      var yData = {}
+      'use strict'
 
-      var boxColor = []
-      var allColors = numeric.linspace(0, 360, xData.length)
-      for (var i = 0; i < xData.length; i++) {{
-        boxColor.push('hsl(' + allColors[i] + ',50%' + ',50%)')
-      }}
+      var finalGrades = {}
+      var finalData = {}
+      var midterm1Score = {}
+      var midterm1Count = {}
+      var midterm2Score = {}
+      var midterm2Count = {}
 
       var data1 = []
       var data2 = []
+      var data3 = []
+      var data4 = []
 
-      for (var i = 0; i < xData.length; i++) {{
+      function generate_rainbow (n) {{
+        var allColors = numeric.linspace(0, 360, n)
+        var result = []
+        for (var i = 0; i < n; i++) {{
+          result.push('hsl(' + allColors[i] + ',50%' + ',50%)')
+        }}
+        return result
+      }}
+
+      var rainbow = generate_rainbow(finalGrades.length)
+
+      for (var i = 0; i < finalGrades.length; i++) {{
         var result = {{
           type: 'box',
           boxpoints: 'all',
-          name: xData[i],
-          y: yData[i],
+          name: finalGrades[i],
+          y: finalData[i],
           marker: {{
-            color: boxColor[i]
+            color: rainbow[i]
           }}
         }}
         data1.push(result)
@@ -75,13 +109,39 @@ def render():
 
       var data2 = [
         {{
-          x: xData,
-          y: yData.map(y => {{
+          x: finalGrades,
+          y: finalData.map(y => {{
             return y.length
           }}),
           type: 'bar',
           marker: {{
-            color: boxColor
+            color: rainbow
+          }}
+        }}
+      ]
+
+      rainbow = generate_rainbow(midterm1Score.length)
+
+      var data3 = [
+        {{
+          x: midterm1Score,
+          y: midterm1Count,
+          type: 'bar',
+          marker: {{
+            color: rainbow
+          }}
+        }}
+      ]
+
+      rainbow = generate_rainbow(midterm2Score.length)
+
+      var data4 = [
+        {{
+          x: midterm2Score,
+          y: midterm2Count,
+          type: 'bar',
+          marker: {{
+            color: rainbow
           }}
         }}
       ]
@@ -92,21 +152,33 @@ def render():
       var layout2 = {{
         title: 'Grade Distribution'
       }}
+      var layout3 = {{
+        title: 'First Midterm Score Distribution'
+      }}
+      var layout4 = {{
+        title: 'Second Midterm Score Distribution'
+      }}
 
       Plotly.newPlot('boxPlot', data1, layout1)
       Plotly.newPlot('gradeDistribution', data2, layout2)
+      Plotly.newPlot('midterm1', data3, layout3)
+      Plotly.newPlot('midterm2', data4, layout4)
 
       window.onresize = function() {{
         Plotly.Plots.resize(document.getElementById('boxPlot'))
         Plotly.Plots.resize(document.getElementById('gradeDistribution'))
+        Plotly.Plots.resize(document.getElementById('midterm1'))
+        Plotly.Plots.resize(document.getElementById('midterm2'))
       }}
     </script>
   </body>
 </html>
 '''
     output = open('index.html','w')
-    x, y = read_from_file('ics_31_course_grades.txt')
-    output.write(html.format(x, y))
+    x_f, y_f = extract_final('ics_31_course_grades.txt')
+    x_m1, y_m1 = extract_midterm('f15.mt1.stats.txt')
+    x_m2, y_m2 = extract_midterm('f15.mt2.stats.txt')
+    output.write(html.format(x_f, y_f, x_m1, y_m1, x_m2, y_m2))
 
 if __name__ == '__main__':
     render()
